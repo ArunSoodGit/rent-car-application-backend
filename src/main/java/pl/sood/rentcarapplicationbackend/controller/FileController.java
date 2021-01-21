8a0220040b4285303501498bba97702581860c41
Arun <arun.sood1998@gmail.com>
2021-01-21 23:19:09 +0100

    nowe funkcjonalności

--- src/main/java/pl/sood/rentcarapplicationbackend/controller/FileController.java
+++ src/main/java/pl/sood/rentcarapplicationbackend/controller/FileController.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import pl.sood.rentcarapplicationbackend.model.File;
 import pl.sood.rentcarapplicationbackend.service.FileStorageService;
 
 import java.io.IOException;
+import java.util.stream.Stream;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -17,5 +19,9 @@ public class FileController {
     @PostMapping("/files")
     public void uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
             storageService.store(file);
+    }
+    @GetMapping("/files")
+    public Stream<File> getFiles() throws IOException {
+        return storageService.getAllFiles();
     }
 }
